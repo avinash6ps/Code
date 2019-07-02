d4fc7a35ab567645ea98f60d4838d1f0fb0f692e
Avinash Singh <avinash@mobifyi.com>
2019-07-02 14:06:06 +0530

    first commit

---
+++ CodeIgniter/application/models/Policymodal.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined("BASEPATH")) {
+    exit("Direct access to this page is not allowed");
+}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class policymodal extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->library('mongo_db');
+
+        $this->load->library('Datatables');
+        $this->load->library('table');
+    }
+
+    
+}
+
+?>
